5b6c7d4c543851e5d95382b27696531ef0127496
Yaamto <73040102+Yaamto@users.noreply.github.com>
2023-07-16 21:29:16 +0200

    [Mock][Api] Mock de l'api ajouté

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PublicUserComponent } from './user/vue/smart/public-user/public-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataService } from './data.service';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { SharedModule } from './shared/shared.module';
+import { AdminUserComponent } from './user/vue/smart/admin-user/admin-user.component';
+import { PublicUserComponent } from './user/vue/smart/public-user/public-user.component';
+import { UserModule } from './user/user.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(DataService),
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
